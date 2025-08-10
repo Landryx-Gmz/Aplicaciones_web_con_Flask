9862baeefe6b2e199405a719772881c0bbb5eb5a
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-10 12:06:36 +0200

    cliente_forma.py creado para formulario con Flask-WTF

---
+++ Flask/cliente_forma.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
+
+
+# Clase que define un formulario web usando Flask-WTF, una extensión de Flask que facilita el manejo de formularios con validación integrada.
+class ClienteForma(FlaskForm):
+    # Tipos de campos que se usarán en el formulario
+    nombre = StringField('Nombre', validators=[DataRequired])  # - StringField: para texto.
+    apellido = StringField('Apellido',
+                           validators=[DataRequired])  # - DataRequired: si el campo está vacío, se genera un error.
+    membresia = IntegerField('Membresia', validators=[DataRequired])  # - IntegerField: para números enteros.
+
+    # Boton
+    gurardar = SubmitField('Guardar')  # - SubmitField: para el botón de envío.
