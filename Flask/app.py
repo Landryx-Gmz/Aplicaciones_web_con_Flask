3afa934f203eab48b611bd0e4e39573de888e694
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-10 10:38:03 +0200

    refactorizacion de index tablas y botones, cientes y conexion ok

--- Flask/app.py
+++ Flask/app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
 
+from cliente_dao import ClienteDAO
+
 app = Flask(__name__)
+
+titulo_app = 'Zona Fit (GYM)'
 
 
 @app.route('/')
 @app.route('/index.html')
 def inicio():
     app.logger.debug('Entramos al path de inicio/')
-    return render_template('index.html')
+    # Recuperamos los clientes de la BD
+    clientes_db = ClienteDAO.seleccionar()
+    return render_template('index.html', titulo=titulo_app, clientes=clientes_db)
 
 
 if __name__ == '__main__':
