a6141fc0aaa9d0465c9272f839a8f5b02768c2af
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-10 08:16:44 +0200

    ruta nueva en app.py

--- Flask/app.py
+++ Flask/app.py
@@ -8,6 +8,8 @@ app = Flask(__name__)  # - Creando la aplicación web.
 # - decorador que le dice a Flask: “Cuando el usuario visite la raíz del sitio (/), ejecuta la función inicio()”.
 
 @app.route('/')  # url: http://localhost:5000/
+# Agregamos otra ruta:
+@app.route('/index.html')  # url http://localhost:5000/index.html
 def inicio():
     app.logger.debug(
         'Entramos en el path de inicio /')  # -Escribe un mensaje en el log en nivel debug. Útil para desarrolladores que quieren saber qué está pasando internamente.
