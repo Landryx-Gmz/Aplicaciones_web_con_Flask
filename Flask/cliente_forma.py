ec1d0e396ce1e7f42390663b853915dbd5a8ed9c
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-10 14:28:18 +0200

    Campo oculto en index y cliente_forma.py

--- Flask/cliente_forma.py
+++ Flask/cliente_forma.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField, HiddenField
 from wtforms.validators import DataRequired
 
 
 # Clase que define un formulario web usando Flask-WTF, una extensión de Flask que facilita el manejo de formularios con validación integrada.
 class ClienteForma(FlaskForm):
     # Tipos de campos que se usarán en el formulario
+    id = HiddenField('id')  # campo oculto con HidedenField()
     nombre = StringField('Nombre', validators=[DataRequired()])  # - StringField: para texto.
     apellido = StringField('Apellido',
                            validators=[DataRequired()])  # - DataRequired: si el campo está vacío, se genera un error.
