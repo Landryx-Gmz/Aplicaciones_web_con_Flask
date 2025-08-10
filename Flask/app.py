330c2ac327b3e3ebd44094259878313420464cd1
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-08-10 12:16:12 +0200

    cliente_forma agregado a app en def inicio

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
+from cliente import Cliente
 from cliente_dao import ClienteDAO
+from cliente_forma import ClienteForma
 
 app = Flask(__name__)
 
@@ -13,7 +15,11 @@ def inicio():
     app.logger.debug('Entramos al path de inicio/')
     # Recuperamos los clientes de la BD
     clientes_db = ClienteDAO.seleccionar()
-    return render_template('index.html', titulo=titulo_app, clientes=clientes_db)
+    # Creamos un formulario de cliente form vacio
+    # creamos un objeto de la clase Cliente()
+    cliente = Cliente()  # Utilizamos el objeto para rellenar los parametros del formulario
+    cliente_forma = ClienteForma(obj=cliente)  # atravez de (obj) indicamos los parametros iniciales
+    return render_template('index.html', titulo=titulo_app, clientes=clientes_db, forma=cliente_forma)
 
 
 if __name__ == '__main__':
